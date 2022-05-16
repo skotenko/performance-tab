059c38e61e623738c5f4e54e3d846a399fc1c175
Serhii Kotenko <s.kotenko@astoundcommerce.com>
2022-05-16 14:17:41 +0300

    add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -17,9 +17,9 @@ const style = () => {
     return (
         gulp
             .src(path.style.src)
-            .pipe(sourcemaps.init())
+            // .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError)) /* compressed | expanded | nested | compact */
-            .pipe(sourcemaps.write(path.style.mapsDest))
+            // .pipe(sourcemaps.write(path.style.mapsDest))
             .pipe(gulp.dest(path.style.dest))
     )
 };
